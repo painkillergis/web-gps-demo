b53a793f246cd467147b15fc8ce694c4b0e5b0db
tyler <tyler@cruftbusters.com>
2021-07-08 23:48:38 -0500

    print current position

--- src/GeolocationDemo.js
+++ src/GeolocationDemo.js
@@ -1,5 +1,53 @@
+import { useEffect, useState } from 'react'
+
 function GeolocationDemo() {
-  return 'geolocation demo'
+  const [currentPosition, setCurrentPosition] = useState('')
+  const { geolocation } = navigator
+
+  useEffect(() => {
+    geolocation.getCurrentPosition((position) => {
+      const {
+        coords: {
+          latitude,
+          longitude,
+          altitude,
+          accuracy,
+          altitudeAccuracy,
+          heading,
+          speed,
+        },
+        timestamp,
+      } = position
+      setCurrentPosition({
+        coords: {
+          latitude,
+          longitude,
+          altitude,
+          accuracy,
+          altitudeAccuracy,
+          heading,
+          speed,
+        },
+        timestamp: timestamp.toString(),
+      })
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>geolocation demo</h1>
+      <h2>current position</h2>
+      {currentPosition && (
+        <p>
+          {Object.entries(currentPosition.coords).map(([key, value]) => (
+            <div>
+              {key}: {value}
+            </div>
+          ))}
+        </p>
+      )}
+    </div>
+  )
 }
 
 export default GeolocationDemo
